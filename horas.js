be433d88516fad78dd36ddbeae89b9177a7a2a19
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-09-05 01:27:51 -0300

    atualizando ...

---
+++ horas.js
@@ -0,0 +1,41 @@
+function init(params) {
+    registrarTarefaEmExecucao();
+}
+
+init();
+
+function registrarTarefaEmExecucao(){
+    process.stdout.write('O que você esta fazendo? ');
+    process.stdin.on('data', data => {
+        let tarefa = data.toString().replace('\n', '');
+        registrar(tarefa, success);
+
+        function success (){
+            console.log("Tarefa registrada com sucesso!");
+            process.exit()
+        }
+    });  
+
+}
+
+function registrar(tarefa, success) {
+    let fs = require('fs');
+
+    let mensagem = getDataTimeFormatada().concat(' - ').concat(tarefa);
+
+    fs.appendFile("registroHoras.txt", mensagem + '\n', function(erro) {
+
+        if(erro) {
+            throw erro;
+        }
+        
+        success();
+    }); 
+}
+
+function getDataTimeFormatada(){
+    let moment = require('moment');
+    let data = new Date();
+    
+    return moment(data).format('DD/MM/YYYY hh:mm:ss');
+}
